bd1f7d497896105c1df699f76e2862da03e68caf
student <a@a.com>
2016-12-07 01:10:39 -0800

    12/6

--- routes/account_routes.js
+++ routes/account_routes.js
@@ -36,4 +36,62 @@ router.get('/', function(req, res){
     }
 });
 
+// Return the add a new account form
+router.get('/add', function(req, res){
+    // passing all the query parameters (req.query) to the insert function instead of each individually
+    account_dal.getAll(function(err,result) {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.render('account/accountAdd', {'account': result});
+        }
+    });
+});
+
+// insert a account record
+router.get('/insert', function(req, res){
+    // simple validation
+    if(req.query.email == null) {
+        res.send('Email must be provided.');
+    }
+    else if(req.query.first_name == null) {
+        res.send('A first name must be selected');
+    }
+    else if(req.query.last_name == null) {
+        res.send('A last name must be selected');
+    }
+    else {
+        // passing all the query parameters (req.query) to the insert function instead of each individually
+        account_dal.insert(req.query, function(err,result) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/account/all');
+            }
+        });
+    }
+});
+
+// Delete a account for the given email
+router.get('/delete', function(req, res){
+    if(req.query.account_id == null) {
+        res.send('account_id is null');
+    }
+    else {
+        account_dal.delete(req.query.account_id, function(err, result){
+            if(err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/account/all');
+            }
+        });
+    }
+});
+
+
 module.exports = router;
